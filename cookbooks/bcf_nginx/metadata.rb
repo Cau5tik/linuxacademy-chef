2612570a6257623ff9a02f4a86a1ebd613856f94
cau5tik <cau5tik@cubenet.ca>
2019-01-25 00:23:43 +0000

    initial commit of bcf_nginx cookbook

--- cookbooks/bcf_nginx/metadata.rb
+++ cookbooks/bcf_nginx/metadata.rb
@@ -1,10 +1,10 @@
 name 'bcf_nginx'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Cau5tik'
+maintainer_email 'cau5tik@cubenet.ca'
 license 'All Rights Reserved'
 description 'Installs/Configures bcf_nginx'
 long_description 'Installs/Configures bcf_nginx'
-version '0.1.0'
+version '1.0.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
--- cookbooks/bcf_nginx/recipes/default.rb
+++ cookbooks/bcf_nginx/recipes/default.rb
@@ -1,5 +1,11 @@
-#
-# Cookbook:: bcf_nginx
-# Recipe:: default
-#
-# Copyright:: 2019, The Authors, All Rights Reserved.
+package "nginx"
+
+service "nginx" do
+    action [:enable, :start]
+end
+
+file "/usr/share/nginx/html/index.html" do
+    content "<h1>Hello World!</h1>"
+    action :create
+    not_if { ::File.exists?("/usr/share/nginx/html/index.html") }
+end
